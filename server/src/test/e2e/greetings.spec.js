b4ec17fd14eace151df5a8ec72cdd487b7ff79b1
Richard Guitter <richard.guitter@bnpparibasfortis.com>
2016-09-08 14:46:04 +0200

    Add expectation to complete the test of GET /hello

--- server/src/test/e2e/greetings.spec.js
+++ server/src/test/e2e/greetings.spec.js
@@ -1,14 +1,25 @@
 'use strict';
 
 describe("GET /hello", () => {
-    it('with undefined "name" in the query string should return the default message', (done) => {
+    const assert = (done, query) => {
         chai
             .request('http://localhost:3000')
             .get('/greetings/hello')
-            .end((err,res) => {
+            .query(query)
+            .end((err, res) => {
                 if (err) done(err);
+                expect(res).to.be.json;
                 expect(res).to.have.status(200);
+                expect(res.body).to.have.all.keys('messages', 'when');
+                expect(res.body.messages).to.equal('Hello ' + ((query && query.name) || 'World'));
+                expect(res.body.when).to.not.be.undefined;
                 done();
             });
+    };
+    it('with undefined "name" in the query string should return the default message "Hello World"', (done) => {
+        assert(done);
+    })
+    it('with "name=richard" in the query string should return the message "Hello richard"', (done) => {
+        assert(done, {name: 'richard'});
     })
 });
