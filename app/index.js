3b34ee81e05700b5b1581343c0cf6c8348e432cb
Richard GUITTER <rguitter@gmail.com>
2016-09-27 16:07:32 +0200

    Refactoring (#44)
    * Merge the root index into the app/index + refactor the npm scripts section to use pre hook + put to travis.yml the logic of exporting test coverage
    
    * Fixing issue relative to CI
    
    * Fix

--- app/index.js
+++ app/index.js
@@ -1,46 +1,59 @@
-// -- Create the application
-const app = require('express')(),
+'use strict';
+
+const program = require('commander'),
+  logging = require('./common/Logging.js'),
+  log = logging.log,
   env = process.env,
-  port = env.app_port || 3000,
-  appName = env.app_name || require('../package.json').name;
+  appName = env.app_name || require('../package.json').name,
+  appVersion = require('../package.json').version;
 
-// -- Configure the log
-const logging = require('./common/Logging.js'),
-  log = logging.log;
-app.use(logging.requestLogger());
-app.use(logging.domainMiddleware());
+program
+  .version(appVersion)
+  .option('start', 'Start the app.')
+  .option('stop', 'Stop the app.')
+  .parse(process.argv);
 
-// -- Configure the endpoints
-const greetingsController = require('./greetings/GreetingsController.js');
-app.use('/greetings', greetingsController);
+if (program.stop) {
+  require('child_process').exec('killall -SIGINT ' + appName, 
+  (error, stdout, stderr) => {
+    if (stdout) log.info(stdout);
+    if (stderr) log.error(error, stderr);
+  });
+}
 
-// -- Configure error logging has to be put after routing.
-app.use(logging.errorLogger);
+if (program.start) {
+  // -- Create the application
+  const app = require('express')(),
+    port = env.app_port || 3000;
 
-const start = () => {
-  process.title = appName;
-  const stop = stopServer(app.listen(port, () => {
-    log.info('App started and listening on port', port);
-  }));
-  process.on('SIGTERM', stop);
-  return stop;
-};
+  // -- Configure the log
+  const logging = require('./common/Logging.js'),
+    log = logging.log;
+  app.use(logging.requestLogger());
+  app.use(logging.domainMiddleware());
 
-const stopServer = (server) => {
-  return () => {
-    log.info('Try to gracefully shutdown.');
-    server.close(() => {
-      log.info('App closed.');
-      process.exit(0);
+  // -- Configure the endpoints
+  const greetingsController = require('./greetings/GreetingsController.js');
+  app.use('/greetings', greetingsController);
+
+  // -- Configure error logging has to be put after routing.
+  app.use(logging.errorLogger);
+
+  const start = () => {
+    process.title = appName;
+    const server = app.listen(port, () => {
+      log.info('App started and listening on port', port);
     });
-    setTimeout(() => {
-      process.exit(1);
-    }, 4000);
+    process.on('SIGTERM', () => {
+      log.info('Try to gracefully shutdown.');
+      server.close(() => {
+        log.info('App closed.');
+        process.exit(0);
+      });
+      setTimeout(() => {
+        process.exit(1);
+      }, 4000);
+    });
   };
-};
-
-module.exports = {
-  app: app,
-  name: appName,
-  start: start
-};
+  start();
+}
--- test/index.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-require('../app/common/ProcessEnv').safeSet('LOG_LEVEL', 'ERROR');
-global.assert = require('chai').assert;
