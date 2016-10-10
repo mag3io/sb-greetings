55209b1b357bd70fc8cc06146f35029bbb195acf
Richard GUITTER <rguitter@gmail.com>
2016-10-10 14:40:51 +0200

    Testing controllers (#46)
    * Add test to the controller
    
    * Add one simple test to the logging module

--- app/greetings/GreetingsController.js
+++ app/greetings/GreetingsController.js
@@ -1,24 +1,33 @@
 'use strict';
 
-const router = require('express').Router(),
-  greetings = require('./GreetingsService'),
+const greetings = require('./GreetingsService'),
   log = require('../common/Logging.js').controllerLog(__filename);
 
-router.get('/hello', (req, res) => {
+const getHello = (req, res) => {
   log().info('Getting "/Hello"');
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({
+  res.type('application/json');
+  res.send({
     messages: greetings.sayHello(req.query.name),
     when: new Date()
-  }, null, 2));
-});
+  });
+};
 
-router.get('/bye', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({
+const getBye = (req, res) => {
+  res.type('application/json');
+  res.send({
     messages: greetings.sayBye(req.query.name),
     when: new Date()
-  }, null, 2));
-});
+  });
+};
 
-module.exports = router;
+const routes = (router) => {
+  router.get('/hello', getHello);
+  router.get('/bye', getBye);
+  return router;
+}
+
+module.exports = {
+  routes: routes,
+  getHello: getHello,
+  getBye: getBye
+}
---
+++ app/greetings/GreetingsController.test.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const sinon = require('sinon'),
+    proxyquire = require('proxyquire'),
+    serviceStub = {};
+
+const sut = proxyquire('./GreetingsController', {'./GreetingsService' : serviceStub});
+
+const test = (sutMethod, stubbing, message, name) => {
+
+    const req = {}, res = {};
+    req.query = {};
+    if (name) req.query.name = name;
+    res.type = sinon.spy();
+    res.send = sinon.spy();
+
+    stubbing(serviceStub);
+    sut[sutMethod](req, res);
+
+    assert.equal(res.type.withArgs('application/json').calledOnce, true);
+    assert.equal(res.send.calledOnce, true);
+    assert.equal(res.send.args[0][0].messages, message);
+    
+};
+
+describe('GreetingsController', function () {
+    
+    describe('GET hello', function () {
+        it('should return "Hello World" if the parameter name is "undefined"', function () {
+            test('getHello', 
+                (serviceStub) => {serviceStub.sayHello = sinon.stub().returns('Hello World');}, 
+                'Hello World');
+        });     
+        it('should return "Hello Richard" if the parameter name is "Richard"', function () {
+             test('getHello', 
+                (serviceStub) => {serviceStub.sayHello = sinon.stub().returns('Hello Richard');}, 
+                'Hello Richard', 
+                'Richard');
+        });
+    });
+
+    describe('GET bye', function () {
+        it('should return "Bye World" if the parameter name is "undefined"', function () {
+            test('getBye', 
+                (serviceStub) => {serviceStub.sayHello = sinon.stub().returns('Hello World');}, 
+                'Bye World');
+        });
+        it('should return "Bye Richard" if the parameter name is "Richard"', function () {
+            test('getBye', 
+                (serviceStub) => {serviceStub.sayHello = sinon.stub().returns('Hello World');}, 
+                'Bye Richard', 
+                'Richard');
+        });
+    });
+
+});
